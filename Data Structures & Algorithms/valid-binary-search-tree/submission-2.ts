bcba3093a1c8fbaf20e9c04eef27575a6499aab3
Supratim Malakar <60012272+supratimmalakar@users.noreply.github.com>
2026-08-17 19:39:44 +0530

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root: TreeNode | null, range?: {min: number, max: number}): boolean {
+        if (root === null) return true;
+
+        if (!range) range = {min: Number.MIN_SAFE_INTEGER, max: Number.MAX_SAFE_INTEGER};
+
+        const {min, max} = range;
+
+        const isRootInRange = root.val > min && root.val < max;
+
+        const l = this.isValidBST(root.left, {min, max: root.val});
+        const r = this.isValidBST(root.right, {min: root.val, max});
+
+        return isRootInRange && l && r;
+    }
+}
