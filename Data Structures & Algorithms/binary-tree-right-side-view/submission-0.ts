80fb5ce2c68f43b9c3c6dd7725f341f17b7b3d3b
Supratim Malakar <60012272+supratimmalakar@users.noreply.github.com>
2026-08-17 14:59:53 +0530

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root: TreeNode | null): number[] {
+        if (root === null) return []
+         let q = [{node: root, level: 0}];
+        let map = []
+        let i = 0;
+        while (i < q.length) {
+            const current = q[i++];
+            const {node, level} = current;
+        map[level] = node.val;
+            if (node?.left) q.push({node: node?.left, level: level + 1});
+            if (node?.right) q.push({node: node?.right, level: level + 1});
+        }
+
+        return map
+    }
+}
