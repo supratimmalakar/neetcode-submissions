909bc209686a242f7fb72f1493fe3f001cba070d
Supratim Malakar <60012272+supratimmalakar@users.noreply.github.com>
2026-08-18 23:54:49 +0530

    Add: delete-node-in-a-bst - submission-1

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-1.ts
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} key
+     * @return {TreeNode}
+     */
+    deleteNode(root: TreeNode | null, key: number): TreeNode {
+        if (root === null) return root;
+
+        if (key === root.val) {
+
+            if (root.right === null && root.left === null) {
+                return null;
+            }
+
+            if (root.right === null && root.left !== null) {
+                return root.left;
+            }
+
+            let curr = root.right;
+
+            while (curr !== null && curr.left !== null) {
+                curr = curr.left;
+            }
+
+            curr.left = root.left;
+
+            const res = root.right;
+
+            root = null;
+
+            return res
+
+        }
+
+        if (key > root.val) {
+            root.right = this.deleteNode(root.right, key)
+        }
+
+        if (key < root.val) {
+            root.left = this.deleteNode(root.left, key)
+        }
+
+        return root
+    }
+}
